fd7971b82f784bdce296ddcecf35558948fdc2dc
javokhirergashev <iqtidorziyosi@gmail.com>
2023-03-01 17:28:10 +0500

    fix

---
+++ languages/en/app.php
@@ -0,0 +1,44 @@
+<?php
+return[
+"slider1" => "Adventure Tours",
+    "sliders1" => "Order now",
+    "slider2" => "Travel around the world!",
+    "sliders2" => "Order now",
+    "service1" => "Affordable Hotels",
+    "service2" => "Unique Views",
+    "service3" => "Constant Communication",
+    "service4" => "Excellent service",
+    "service5" => "Delicious food",
+    "service6" => "Taxi service",
+    "about" => "About Us",
+    "about1" => "Any country with good tourism is possible. We can create facilities for you. We have adventurous trips for you at reasonable prices.",
+    "about2" => "You can experience the adventure of your dreams by purchasing the tour package that suits you best and booking the tour.",
+
+    "galleryinfo" => "Our Gallery",
+    "hotels" => "Hotels",
+    "tours" => "Tour Packages",
+    "counter1" => "Flight Booking",
+    "counter2" => "Great trip",
+    "counter3" => "Book Cruises",
+    "counter4" => "Ticket Booking",
+    "category" => "Type package categories",
+    "contact1" => "Contacts",
+
+// Contact page
+    "contactinfo" => "Contact",
+    'name'=>"Name",
+    'surname' => "Family",
+    'email' => "Email Address",
+    'tel' => "Phone",
+    'message' => "Send Message",
+    'send' => "Send",
+    'category1' => "Select type package",
+    "data" => "date.month.year",
+    "location" => "Address",
+    "contactp" => "Contact us and travel the world.",
+
+
+
+]
+
+?>
